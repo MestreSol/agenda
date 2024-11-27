205e088b36f57248595dad64cb4b4736a4e7561f
MestreSol <mb.muitobosta@gmail.com>
2024-11-27 13:22:19 -0300

    Update Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, RouteProps } from 'react-router-dom';
-import Home from './pages/home';
-import About from './pages/about';
-
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  RouteProps
+} from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
 
 function App() {
   return (
@@ -12,7 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
